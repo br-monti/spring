2e3d5edc4c1a879759e9b7efb3fc7f937c28be70
Bruno <bruno.smsouza@gmail.com>
2020-03-22 14:26:08 -0300

    Aula 7,4. Primeira versão em produção

--- algamoney-api/src/main/java/com/example/algamoney/api/resource/TokenResource.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/resource/TokenResource.java
@@ -22,7 +22,7 @@ public class TokenResource {
 	@DeleteMapping("/revoke")
 	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
 		Cookie cookie = new Cookie("refreshToken", null);
-		cookie.setHttpOnly(true);
+		cookie.setHttpOnly(true); // TODO: Em produção será true
 		cookie.setSecure(algamoneyApiProperty.getSeguranca().isEnableHttps());
 		cookie.setPath(req.getContextPath() + "/oauth/token");
 		cookie.setMaxAge(0);
